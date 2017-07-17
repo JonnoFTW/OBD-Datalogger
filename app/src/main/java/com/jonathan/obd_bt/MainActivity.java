f04754afa7228b52162ffcb9bf8d9dfe6ee14d0d
Jonathan Mackenzie <jonmac1@gmail.com>
2017-07-17 15:06:42 +0930

    did stuff I forget what

--- app/src/main/java/com/jonathan/obd_bt/MainActivity.java
+++ app/src/main/java/com/jonathan/obd_bt/MainActivity.java
@@ -8,7 +8,6 @@ import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
-import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -36,12 +35,9 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
